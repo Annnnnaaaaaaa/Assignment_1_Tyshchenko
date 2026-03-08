68c23488de168436c28d3add487fa4f7434f196d
Annnnnaaaaaaa <dnipro.anna@gmail.com>
2026-03-08 23:55:18 +0200

    Initial commit

---
+++ Assignment 1_Tyshchenko/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Assignment_1_Tyshchenko;
+
+public class Program
+{
+    static void Main()
+    {
+        string numbers = ""; // буфер для цифр
+        var[] postfix = new var[100];
+        int res = 0;
+        Stack myStack = new Stack();
+
+        string sm_string = "1+  3*(  2+ 4)";
+
+        for (int i = 0; i < sm_string.Length; i++)
+        {
+            if (sm_string[i] is int)
+            {
+                numbers += sm_string[i];
+                
+                if (i == sm_string.Length - 1 || sm_string[i] is not int)
+                {
+                    i++;
+                    postfix.Add(numbers);
+                    numbers = "";
+                }
+            }
+            else
+            {
+                myStack.Pull(sm_string[i]);
+            }
+        }
+        
+        for (int i = 0; i<postfix.Length; i++)
+        {
+            if (postfix[i] is not int)
+            {
+                
+            }
+        }
+        
+        
+        
+        // // 1. Створення об'єкта класу Stack
+        // Stack myStack = new Stack();
+        //
+        // // 2. Додавання елементів (Push)
+        // myStack.Push("Перший");
+        // myStack.Push("Другий");
+        // myStack.Push("Третій");
+        //
+        // // 3. Вилучення елементів (Pull)
+        // // Оскільки це стек (LIFO), "Третій" вийде першим
+        // string item1 = myStack.Pull(); 
+        // Console.WriteLine(item1); // Виведе: Третій
+        //
+        // string item2 = myStack.Pull();
+        // Console.WriteLine(item2); // Виведе: Другий
+        //
+        // // 4. Демонстрація порожнього стека
+        // myStack.Pull(); // Витягли "Перший"
+        // var empty = myStack.Pull(); // Тут поверне null, як прописано в логіці
+    }
+}
+    
+    
---
+++ Assignment 1_Tyshchenko/Stack.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment_1_Tyshchenko;
+
+public class Stack
+{
+    private const int Capacity = 50;
+    private string[] _array = new string[Capacity];
+    private int _pointer;
+    
+    public void Push(string value)
+    {
+        if (_pointer == _array.Length)
+        {
+            // this code is raising an exception about reaching stack limit
+            throw new Exception("Stack overflowed");
+        }
+        _array[_pointer] = value;
+        _pointer++;
+    }
+    public string Pull()
+    {
+        if (_pointer == 0)
+        {
+            //you can also raise an exception here, but we're simple returning nothing
+            return null;
+        }
+        _pointer--;
+        var value = _array[_pointer];
+        return value;
+    }
+}
